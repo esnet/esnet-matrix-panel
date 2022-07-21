eb5634e206b9a905e0d711a9d404b86c7b220547
Katrina H. Turner <46706381+KatrinaTurner@users.noreply.github.com>
2022-07-21 08:50:06 -1000

    replace custom color parser with grafana themes

--- dist/module.js
+++ dist/module.js
@@ -16321,8 +16321,8 @@ function createViz(elem, id, height, data, theme, options) {
   txtLength = options.txtLength,
       txtSize = options.txtSize / 100,
       //convert this val to EM scaling 90 = .9em 100 = 1em ... etc
-  nullColor = fixColor(options.nullColor),
-      defaultColor = fixColor(options.defaultColor),
+  nullColor = theme.visualization.getColorByName(options.nullColor),
+      defaultColor = theme.visualization.getColorByName(options.defaultColor),
       linkURL = options.url,
       urlVar1 = options.urlVar1,
       urlVar2 = options.urlVar2; // urlOther = options.urlOther,
@@ -16414,7 +16414,7 @@ function createViz(elem, id, height, data, theme, options) {
   //on mouse out the div will move back to 0,0 so
   //as not to be covering other boxes we want to hover
 
-  var div = _d3_min_js__WEBPACK_IMPORTED_MODULE_1__.select('body').append('div').attr('class', 'tooltip').style('background-color', theme.colors.bg1).style('font-family', theme.typography.fontFamily.sansSerif).style('font-color', theme.colors.text).style('box-shadow', '3px 3px 6px lightgray').style('padding', '5px').style('opacity', 0); // append the svg object to the body of the page
+  var div = _d3_min_js__WEBPACK_IMPORTED_MODULE_1__.select('body').append('div').attr('class', 'tooltip').style('background-color', theme.colors.background.primary).style('font-family', theme.typography.fontFamily.sansSerif).style('font-color', theme.colors.text.primary).style('box-shadow', '3px 3px 6px lightgray').style('padding', '5px').style('opacity', 0); // append the svg object to the body of the page
 
   var svgClass = "svg-".concat(id);
   var svg = _d3_min_js__WEBPACK_IMPORTED_MODULE_1__.select(elem).append('svg').attr('id', svgClass).attr('width', width + margin.left + margin.right).attr('height', height + margin.top + margin.bottom).append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')'); // Build X scales and axis:
@@ -16428,7 +16428,7 @@ function createViz(elem, id, height, data, theme, options) {
   var y = _d3_min_js__WEBPACK_IMPORTED_MODULE_1__.scaleBand().range([height, 0]).domain(rowNames.slice().reverse()).padding(cellPadding);
   svg.append('g').call(_d3_min_js__WEBPACK_IMPORTED_MODULE_1__.axisLeft(y)).select('.domain').remove(); //the scale bands have created the labels on the axis now we need to make sure the styles are set and add the hover events
 
-  svg.selectAll('text').attr('font-size', txtSize + 'em').style('font-family', theme.typography.fontFamily.sansSerif).attr('fill', theme.colors.text).call(truncateLabel, maxTxtLength).on('mouseover', function (event, d) {
+  svg.selectAll('text').attr('font-size', txtSize + 'em').style('font-family', theme.typography.fontFamily.sansSerif).attr('fill', theme.colors.text.primary).call(truncateLabel, maxTxtLength).on('mouseover', function (event, d) {
     div.html(d); //to center the tooltip appropriately we need to find the rendered width of both the
     //the box they hovered and of the tooltip with the text in it.
 
@@ -16590,117 +16590,6 @@ function prepData(data, src, target, val) {
 }
 /**
  *
- * @param {string} color the color returned by grafana ie light-green
- * @returns {string} the hex of the color
- */
-
-
-function fixColor(color) {
-  switch (color) {
-    case 'dark-green':
-      color = '#1A7311';
-      break;
-
-    case 'semi-dark-green':
-      color = '#36872D';
-      break;
-
-    case 'light-green':
-      color = '#73BF68';
-      break;
-
-    case 'super-light-green':
-      color = '#96D88C';
-      break;
-
-    case 'dark-yellow':
-      color = 'rgb(207, 159, 0)';
-      break;
-
-    case 'semi-dark-yellow':
-      color = 'rgb(224, 180, 0)';
-      break;
-
-    case 'light-yellow':
-      color = 'rgb(250, 222, 42)';
-      break;
-
-    case 'super-light-yellow':
-      color = 'rgb(255, 238, 82)';
-      break;
-
-    case 'dark-red':
-      color = 'rgb(173, 3, 23)';
-      break;
-
-    case 'semi-dark-red':
-      color = 'rgb(196, 22, 42)';
-      break;
-
-    case 'light-red':
-      color = 'rgb(242, 73, 92)';
-      break;
-
-    case 'super-light-red':
-      color = 'rgb(255, 115, 131)';
-      break;
-
-    case 'dark-blue':
-      color = 'rgb(18, 80, 176)';
-      break;
-
-    case 'semi-dark-blue':
-      color = 'rgb(31, 96, 196)';
-      break;
-
-    case 'light-blue':
-      color = 'rgb(87, 148, 242)';
-      break;
-
-    case 'super-light-blue':
-      color = 'rgb(138, 184, 255)';
-      break;
-
-    case 'dark-orange':
-      color = 'rgb(229, 84, 0)';
-      break;
-
-    case 'semi-dark-orange':
-      color = 'rgb(250, 100, 0)';
-      break;
-
-    case 'light-orange':
-      color = 'rgb(255, 152, 48)';
-      break;
-
-    case 'super-light-orange':
-      color = 'rgb(255, 179, 87)';
-      break;
-
-    case 'dark-purple':
-      color = 'rgb(124, 46, 163)';
-      break;
-
-    case 'semi-dark-purple':
-      color = 'rgb(143, 59, 184)';
-      break;
-
-    case 'light-purple':
-      color = 'rgb(184, 119, 217)';
-      break;
-
-    case 'super-light-purple':
-      color = 'rgb(202, 149, 229)';
-      break;
-
-    default:
-      break;
-  }
-
-  return color;
-}
-/**
- *
  * @param {*} data Data for the chord diagram
  * @param {*} id The panel id
  * @param {string} src The data series that will act as the source
@@ -16712,7 +16601,7 @@ function fixColor(color) {
 
 
 function matrix(data, id, height, options) {
-  var theme = (0,_grafana_ui__WEBPACK_IMPORTED_MODULE_3__.useTheme)(); // some react related voodoo
+  var theme = (0,_grafana_ui__WEBPACK_IMPORTED_MODULE_3__.useTheme2)(); // some react related voodoo
 
   var ref = (0,_useD3_js__WEBPACK_IMPORTED_MODULE_0__.useD3)(function (svg) {
     createViz(svg, id, height, data, theme, options);
