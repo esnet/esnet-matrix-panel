372e503f4008bf23ad7c33710d323a1386922039
Katrina H. Turner <46706381+KatrinaTurner@users.noreply.github.com>
2025-06-13 10:14:33 -1000

    clean up

--- src/EsnetMatrix.tsx
+++ src/EsnetMatrix.tsx
@@ -16,7 +16,7 @@ interface Props extends PanelProps<MatrixOptions> {
 export const EsnetMatrix: React.FC<Props> = ({ options, data, width, height, id }) => {
   // console.log(data);
   const theme = useTheme2();
-  console.log(options);
+  // console.log(options);
   const parsedData = parseData(data, options, theme);
   try {
     if (parsedData.data === 'too many inputs') {
